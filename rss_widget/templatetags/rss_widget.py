abc10adf790a1bc5723e01d901c82e95511fb94c
Matt Caldwell <matt.caldwell@gmail.com>
2013-12-04 14:47:22 -0500

    Update rss_widget.py
    cache RSS_WIDGET settings dict

--- rss_widget/templatetags/rss_widget.py
+++ rss_widget/templatetags/rss_widget.py
@@ -6,8 +6,9 @@ register = Library()
 
 @register.simple_tag
 def rss_widget(template_name='', feed_url=''):
-    template_name = template_name or getattr(settings, 'RSS_WIDGET', {}).get('template_name', '')
-    feed_url = feed_url or getattr(settings, 'RSS_WIDGET', {}).get('feed_url', '')
+    _settings = getattr(settings, 'RSS_WIDGET', {})
+    template_name = template_name or _settings.get('template_name', '')
+    feed_url = feed_url or _settings.get('feed_url', '')
     feed = feedparser.parse(feed_url)
     template = loader.get_template(template_name)
     return template.render(Context({'feed': feed}))
